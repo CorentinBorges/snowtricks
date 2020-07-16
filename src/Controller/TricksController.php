37f2caeda281283128b2aadc11ff8cf46950265c
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-07-16 13:20:41 +0200

    Figure class added+ show page without comments done

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,13 @@ class TricksController extends AbstractController
         return $this->render('tricks/index.html.twig', [
             'controller_name' => 'TricksController',
         ]);
+    }
+
+    /**
+     * @Route("/trick/{id}",name="app_show")
+     */
+    public function show(Figure $figure)
+    {
+        return $this->render("tricks/show;html.twig");
     }
 }
