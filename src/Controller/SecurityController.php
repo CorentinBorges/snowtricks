3b795c4cbc3dcc035e5ed9946c5dfd00524f4e72
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-07-30 17:33:16 +0200

    sign in works with token and mail (no refactor yet)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Token;
 use App\Entity\User;
 use App\Form\UserRegistrationFormType;
+use App\Repository\TokenRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -54,7 +60,9 @@ class SecurityController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $userModel = $form->getData();
 
+
             $user = new User();
+
             $user
                 ->setRoles(['ROLE_ADMIN'])
                 ->setEmail($form['email']->getData())//TODO: demander à karim pourquoi setEmail($userModel['email'] ne fonctionne pas
@@ -62,13 +70,31 @@ class SecurityController extends AbstractController
                 ->setPassword($passwordEncoder->encodePassword($user,$form['password']->getData()))
                 ->setIsValid(false);
             $entityManager->persist($user);
+
+            $token = new Token();
+
+            $now = new \DateTime();
+            $time = new \DateTime();
+            $expireTime = $time->modify('+1 hour');
+            $token
+                ->setName(uniqid() . uniqid())
+                ->setUser($user)
+                ->setCreatedAt($now)
+                ->setExpiredAt($expireTime)
+                ->setIsUsed(false);
+            $entityManager->persist($token);
             $entityManager->flush();
 
             $email = (new Email())
-                ->from('corentin1309@gmail.com')
+                ->from('cb.corentinborges@gmail.com')
                 ->to($form['email']->getData())
-                ->subject("Snowricks: confirmation par mail")
-                ->text('mail de confirmation');
+                ->subject("Snowtricks: confirmation par mail")
+                ->text('Mail de confirmation')
+                ->html('
+                    <h1>Confirmation d\'inscription snowtricks</h1>
+                    <p>Veuillez confirmer votre inscription en cliquant sur 
+                    <a href="https://localhost:8000/confirmation/'.$token->getName().'"> Ce lien</a>
+                    </p>');
             $mailer->send($email);
 
             $this->addFlash('registerSuccess',"Un mail de confirmation vous à été envoyé à l'adresse ".$form['email']->getData());
@@ -79,7 +105,26 @@ class SecurityController extends AbstractController
             'registrationForm' => $form->createView(),
 
         ]);
+    }
+
+    /**
+     * @Route("/confirmation/{name}",name="app_confirmUser")
+     * @param Token $token
+     * @return Response
+     */
+    public function confirmUser(Token $token,EntityManagerInterface $entityManager,Request $request)
+    {
+        if ($token->getIsUsed()) {
+          throw new NotFoundHttpException();
+        }
+
+        $user = $token->getUser();
+        $user->setIsValid(true);
+        $token->setIsUsed(true);
+        $entityManager->flush();
 
 
+
+        return $this->render('security/confirmUser.html.twig');
     }
 }
--- src/Entity/Token.php
+++ src/Entity/Token.php
@@ -107,4 +107,5 @@ class Token
 
         return $this;
     }
+
 }
